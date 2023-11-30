9c098458d08bf3f2c1bdd5c958d28691e64436de
Amin2-hub <wnstjd05@naver.com>
2023-11-30 12:34:40 +0900

    11.30-2

---
+++ vue/vue-test/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <VueComponent />
+</template>
+
+<script>
+import VueComponent from './components/VueComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    VueComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-test/src/components/VueComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <label><input type="text" v-model="msg"></label>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name : 'VueConponent', //컴포넌트이름 - 없어도 동작에 문제없긴함 관리용으로 이름적어두는편
+    data(){
+        return{
+            msg : '2023-11-30'   
+            //기본값
+        }
+    }
+}
+
+</script>
+<style>
+    p { 
+        background-color: rgb(229, 184, 247);
+    }
+</style>
